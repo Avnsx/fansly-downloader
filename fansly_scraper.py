44e77e49d48a4c2d26793eb3f637b82717a4753e
Mika C <59329994+Avnsx@users.noreply.github.com>
2022-08-28 21:29:31 +0200

    new features and changes
    changes:
    + configuration "update_recent_download" can now also be set to "Auto"
    + adjusted scraper for fansly api version 3
    
    added configuration settings for:
    + seperating messages or previews into subfolders
     > "seperate _messages/_previews" can be set to "True" or "False" 
    + naming files by date posted (https://github.com/Avnsx/fansly/issues/28 - thanks @pawnstar81)
     > "naming_convention" is now supported with values "DatePost" or "Standard"

--- fansly_scraper.py
+++ fansly_scraper.py
@@ -31,6 +31,7 @@ if len(config.read('config.ini')) != 1:
     exit()
 
 try:
+    # I'm aware that I could've used config.getint(), getfloat, getboolean etc.
     mycreator = config['TargetedCreator']['Username']
     mytoken = config['MyAccount']['Authorization_Token']
     myuseragent = config['MyAccount']['User_Agent']
@@ -40,7 +41,7 @@ try:
     openwhenfinished = config['Options']['Open_Folder_When_Finished'].capitalize()
     naming = config['Options']['naming_convention'].capitalize()
     seperate_messages = config['Options']['seperate_messages'].capitalize()
-    seperate_previews = config['Options']['seperate_messages'].capitalize()
+    seperate_previews = config['Options']['seperate_previews'].capitalize()
     curent_ver = config['Other']['version']
 except (KeyError, NameError) as e:
     output(2,'\n [2]ERROR','<red>', f'"{e}" is missing or malformed in the configuration file!\n{21*" "}Read the Wiki > Explanation of provided programs & their functionality > config.ini')
@@ -62,12 +63,12 @@ for x in show,previews,openwhenfinished,seperate_messages,seperate_previews:
         exit()
 
 if remember != 'True' and remember != 'False' and remember != 'Auto':
-    output(2,'\n [4]ERROR','<red>', f'"{remember}" is malformed in the configuration file! This value can only be True, False or Auto\n{21*" "}Read the Wiki > Explanation of provided programs & their functionality > config.ini')
+    output(2,'\n [5]ERROR','<red>', f'"{remember}" is malformed in the configuration file! This value can only be True, False or Auto\n{21*" "}Read the Wiki > Explanation of provided programs & their functionality > config.ini')
     input('\nPress any key to close ...')
     exit()
 
 if naming != 'Standard' and naming != 'Datepost':
-    output(2,'\n [4]ERROR','<red>', f'"{naming}" is malformed in the configuration file! This value can only be Standard or DatePost\n{21*" "}Read the Wiki > Explanation of provided programs & their functionality > config.ini')
+    output(2,'\n [6]ERROR','<red>', f'"{naming}" is malformed in the configuration file! This value can only be Standard or DatePost\n{21*" "}Read the Wiki > Explanation of provided programs & their functionality > config.ini')
     input('\nPress any key to close ...')
     exit()
 
@@ -79,18 +80,18 @@ def open_file(myfile):
         elif os_v == 'Darwin':subprocess.Popen(['open', myfile])
         else:
             if openwhenfinished == 'True':
-                output(2,'\n [5]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; if this happens again turn Open_Folder_When_Finished to "False" in the file "config.ini".\n{21*" "}Will try to continue ...')
+                output(2,'\n [7]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; if this happens again turn Open_Folder_When_Finished to "False" in the file "config.ini".\n{21*" "}Will try to continue ...')
                 s(5)
             else:
-                output(2,'\n [6]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; this happend while trying to do an required update!\n{21*" "}Please update, by either opening "{myfile}" manually or downloading the new version from github.com/Avnsx/Fansly')
+                output(2,'\n [8]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; this happend while trying to do an required update!\n{21*" "}Please update, by either opening "{myfile}" manually or downloading the new version from github.com/Avnsx/Fansly')
                 s(30)
                 exit()
     except:
         if openwhenfinished == 'True':
-            output(2,'\n [7]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; if this happens again turn Open_Folder_When_Finished to "False" in the file "config.ini".\n{21*" "}Will try to continue ...')
+            output(2,'\n [9]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; if this happens again turn Open_Folder_When_Finished to "False" in the file "config.ini".\n{21*" "}Will try to continue ...')
             s(5)
         else:
-            output(2,'\n [8]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; this happend while trying to do an required update!\n{21*" "}Please update, by either opening "{myfile}" manually or downloading the new version from github.com/Avnsx/Fansly')
+            output(2,'\n [10]ERROR','<red>', f'Fansly scraper could not open "{myfile}"; this happend while trying to do an required update!\n{21*" "}Please update, by either opening "{myfile}" manually or downloading the new version from github.com/Avnsx/Fansly')
             s(30)
             exit()
 
@@ -106,16 +107,22 @@ try:
         s(10)
         exit()
 except requests.exceptions.ConnectionError as e:
-    output(2,'\n [9]ERROR','<red>', 'Update check failed, due to no internet connection!')
+    output(2,'\n [11]ERROR','<red>', 'Update check failed, due to no internet connection!')
     print('\n'+str(e))
     input('\nPress any key to close ...')
     exit()
 except Exception as e:
-    output(2,'\n [10]ERROR','<red>', 'Update check failed, will try to continue ...')
+    output(2,'\n [12]ERROR','<red>', 'Update check failed, will try to continue ...')
     print('\n'+str(e))
     s(3)
     pass
 
+F, c ='fNs', mytoken
+if c[-3:]==F:c=c.rstrip(F)
+A,B,C=['']*len(c),7,0
+for D in range(B):
+    for E in range(D,len(A),B):A[E]=c[C];C+=1
+mytoken = ''.join(A)
 
 headers = {
     'Accept': 'application/json, text/plain, */*',
@@ -126,20 +133,20 @@ headers = {
 }
 
 try:
-    raw_req = sess.get('https://apiv2.fansly.com/api/v1/account?usernames='+mycreator, headers=headers)
+    raw_req = sess.get('https://apiv3.fansly.com/api/v1/account?usernames='+mycreator, headers=headers)
     acc_req = raw_req.json()['response'][0]
     creator_id = acc_req['id']
 except KeyError as e:
     if raw_req.status_code == 401:
-        output(2,'\n [11]ERROR','<red>', 'API returned unauthorized. This is most likely because of a wrong authorization token, in the configuration file.')
+        output(2,'\n [13]ERROR','<red>', 'API returned unauthorized. This is most likely because of a wrong authorization token, in the configuration file.')
         print(f'{21*" "}Used authorization token: "'+mytoken+'"')
-    else:output(2,'\n [12]ERROR','<red>', 'Bad response from fansly API. Please make sure your configuration file is not malformed.')
+    else:output(2,'\n [14]ERROR','<red>', 'Bad response from fansly API. Please make sure your configuration file is not malformed.')
     print('\n'+str(e))
     print(raw_req.text)
     input('\nPress any key to close ...')
     exit()
 except IndexError as e:
-    output(2,'\n [13]ERROR','<red>', 'Bad response from fansly API. Please make sure your configuration file is not malformed; most likely misspelled the creator name.')
+    output(2,'\n [15]ERROR','<red>', 'Bad response from fansly API. Please make sure your configuration file is not malformed; most likely misspelled the creator name.')
     print('\n'+str(e))
     print(raw_req.text)
     input('\nPress any key to close ...')
@@ -153,7 +160,6 @@ total_photos = acc_req['timelineStats']['imageCount']
 total_videos = acc_req['timelineStats']['videoCount']
 
 output(1,' Info','<light-blue>','Targeted creator: "'+mycreator+'"')
-output(1,' Info','<light-blue>','Using authorisation token: "'+mytoken+'"')
 output(1,' Info','<light-blue>','Using user-agent: "'+myuseragent[:28]+' [...] '+myuseragent[-35:]+'"')
 output(1,' Info','<light-blue>','Open download folder when finished, is set to: "'+openwhenfinished+'"')
 output(1,' Info','<light-blue>','Downloading files marked as preview, is set to: "'+previews+'"')
@@ -165,10 +171,7 @@ if randint(1,100) <= 19:
     output(4,'\n lnfo','<light-red>', f"Fansly scraper was downloaded {tot_downs} times, but only {round(requests.get('https://api.github.com/repos/avnsx/fansly', headers={'User-Agent':'Fansly Scraper'}).json()['stargazers_count']/tot_downs*100, 2)} % of You(!) have starred it\n{19*' '}Stars directly influence my willingness to continue maintaining the project\n{23*' '}Help the repository grow today, by leaving a star on it!")
     s(15)
 
-recent_photobyte_hashes=[]
-recent_videobyte_hashes=[]
-
-basedir=mycreator+'_fansly'
+recent_photobyte_hashes, recent_videobyte_hashes, basedir = [], [], mycreator+'_fansly'
 
 def process_img(filePath):
     recent_photobyte_hashes.append(str(imagehash.average_hash(Image.open(filePath))))
@@ -196,7 +199,7 @@ if remember == 'True':
         if basedir:
             output(1,' Info','<light-blue>', f'Chose folder path {basedir}')
         else:
-            output(2,'\n [14]ERROR','<red>', f'Could not register your chosen folder path, please start all over again. Closing in 30 seconds')
+            output(2,'\n [16]ERROR','<red>', f'Could not register your chosen folder path, please start all over again. Closing in 30 seconds')
             s(30)
             exit()
 
@@ -210,12 +213,12 @@ if remember == 'True':
                 if os.path.isdir(p_path):
                     output(1,' Info','<light-blue>', f"Hashing {mycreator}'s recently downloaded pictures from {p_path} ...")
                     for el in os.listdir(p_path):
-                        list_of_futures.append(executor.submit(process_img, f'{ p_path }/{ el }'))
+                        list_of_futures.append(executor.submit(process_img, f'{p_path}/{el}'))
                         
                 if os.path.isdir(x_path + '/Videos'):
                     output(1,' Info','<light-blue>', f"Hashing {mycreator}'s recently downloaded videos from {v_path} ...")
                     for el in os.listdir(v_path):
-                        list_of_futures.append(executor.submit(process_vid, f'{ v_path }/{ el }'))
+                        list_of_futures.append(executor.submit(process_vid, f'{v_path}/{el}'))
 
     concurrent.futures.wait(list_of_futures)
 
@@ -248,16 +251,11 @@ else:
                 os.makedirs(basedir+'/Previews/Videos', exist_ok = True)
     except Exception:
         print('\n'+traceback.format_exc())
-        output(2,'\n [15]ERROR','<red>', 'Creating download directories ... Please copy & paste this on GitHub > Issues & provide a short explanation.')
+        output(2,'\n [17]ERROR','<red>', 'Creating download directories ... Please copy & paste this on GitHub > Issues & provide a short explanation.')
         input('\nPress any key to close ...')
         exit()
 
-pic_count=1
-vid_count=1
-duplicates=0
-recent=0
-photobyte_hashes=[]
-videobyte_hashes=[]
+pic_count, vid_count, duplicates, recent, photobyte_hashes, videobyte_hashes = 1, 1, 0, 0, [], []
 def sort_download(filename,filebytes, directoryName):
     global pic_count, vid_count, duplicates, recent
     win_comp_name=str(re.sub(r'[\\/:*?"<>|]', '', repr(filename).replace("'",''))).replace('..','.')
@@ -271,7 +269,7 @@ def sort_download(filename,filebytes, directoryName):
                 else:
                     prefix = f"{pic_count}-{randints}_"
                 if show == 'True':output(1,' Info','<light-blue>', f"Downloading Image '{win_comp_name}'")
-                with open(f"{ directoryName }/Pictures/{prefix}{win_comp_name}", 'wb') as f:f.write(filebytes)
+                with open(f"{directoryName}/Pictures/{prefix}{win_comp_name}", 'wb') as f:f.write(filebytes)
                 photobyte_hashes.append(photohash)
                 pic_count+=1
             else:duplicates+=1
@@ -285,17 +283,17 @@ def sort_download(filename,filebytes, directoryName):
                 else:
                     prefix = f"{vid_count}-{randints}_"
                 if show == 'True':output(1,' Info','<light-blue>', f"Downloading Video '{win_comp_name}'")
-                with open(f"{ directoryName }/Videos/{prefix}{win_comp_name}", 'wb') as f:f.write(filebytes)
+                with open(f"{directoryName}/Videos/{prefix}{win_comp_name}", 'wb') as f:f.write(filebytes)
                 videobyte_hashes.append(videohash)
                 vid_count+=1
             else:duplicates+=1
         else:recent+=1
     else:
-        output(2,'\n [16]ERROR','<red>', 'Unknown filetype: "'+str(filename[-7:])+'" please report this on GitHub > Issues & provide a short explanation; continuing without that file ...')
+        output(2,'\n [18]ERROR','<red>', 'Unknown filetype: "'+str(filename[-7:])+'" please report this on GitHub > Issues & provide a short explanation; continuing without that file ...')
 
 # scrape messages
 group_id = None
-groups = sess.get('https://apiv2.fansly.com/api/v1/group', headers=headers).json()['response']['groups']
+groups = sess.get('https://apiv3.fansly.com/api/v1/group', headers=headers).json()['response']['groups']
 for x in range(len(groups)):
     if groups[x]['users'][0]['userId'] == creator_id:
         group_id = groups[x]['id']
@@ -306,19 +304,19 @@ if group_id:
     msg_cursor = None
     while True:
         if seperate_messages == 'True':
-            directory_name = f'{ basedir }/messages'
+            directory_name = f'{basedir}/messages'
         else:
             directory_name = f'{basedir}'
         if seperate_previews == 'True':
-            preview_directory_name = f'{ directory_name }/previews'
+            preview_directory_name = f'{directory_name}/previews'
         else:
             preview_directory_name = directory_name
         if not msg_cursor:
             output(1,' Info','<light-blue>', f'Inspecting message: {group_id}')
-            resp = sess.get('https://apiv2.fansly.com/api/v1/message', headers=headers, params=(('groupId', group_id),('limit', '50'),)).json()
+            resp = sess.get('https://apiv3.fansly.com/api/v1/message', headers=headers, params=(('groupId', group_id),('limit', '50'),)).json()
         else:
             output(1,' Info','<light-blue>', f'Inspecting message: {msg_cursor}')
-            resp = sess.get('https://apiv2.fansly.com/api/v1/message', headers=headers, params=(('groupId', group_id),('before', msg_cursor),('limit', '50'),)).json()
+            resp = sess.get('https://apiv3.fansly.com/api/v1/message', headers=headers, params=(('groupId', group_id),('before', msg_cursor),('limit', '50'),)).json()
         try:
             for x in resp['response']['accountMedia']:
                 # set filename
@@ -326,7 +324,7 @@ if group_id:
                     ts = int(x['createdAt'])
                     file_datetime = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H-%M-%S')
                     file_id = x['id']
-                    file_name = f"{ file_datetime } { file_id }"
+                    file_name = f"{file_datetime} {file_id}"
                 else:
                     file_name = f"{mycreator}"
                 # message media previews
@@ -355,7 +353,7 @@ if group_id:
             except IndexError:break # break if end is reached
             except Exception:
                 print('\n'+traceback.format_exc())
-                output(2,'\n [17]ERROR','<red>', 'Please copy & paste this on GitHub > Issues & provide a short explanation.')
+                output(2,'\n [19]ERROR','<red>', 'Please copy & paste this on GitHub > Issues & provide a short explanation.')
                 input('\nPress any key to close ...')
                 exit()
         except KeyError:
@@ -367,16 +365,16 @@ output(1,' Info','<light-blue>','Started profile media download; this could take
 cursor = 0
 while True:
     if seperate_messages == 'True':
-        directory_name = f'{ basedir }/timeline'
+        directory_name = f'{basedir}/timeline'
     else:
         directory_name = f'{basedir}'
     if seperate_previews == 'True':
-        preview_directory_name = f'{ directory_name }/previews'
+        preview_directory_name = f'{directory_name}/previews'
     else:
         preview_directory_name = directory_name
     if cursor == 0:output(1,' Info','<light-blue>', f'Inspecting most recent page')
     else:output(1,' Info','<light-blue>', f'Inspecting page: {cursor}')
-    response = sess.get('https://apiv2.fansly.com/api/v1/timeline/'+creator_id+'?before='+str(cursor)+'&after=0', headers=headers)
+    response = sess.get('https://apiv3.fansly.com/api/v1/timeline/'+creator_id+'?before='+str(cursor)+'&after=0', headers=headers)
     try:
         for x in response.json()['response']['accountMedia']:
             # set filename
@@ -384,7 +382,7 @@ while True:
                 ts = int(x['createdAt'])
                 file_datetime = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H-%M-%S')
                 file_id = x['id']
-                file_name = f"{ file_datetime } { file_id }"
+                file_name = f"{file_datetime} {file_id}"
             else:
                 file_name = f"{mycreator}"
             # previews
@@ -413,11 +411,11 @@ while True:
         except IndexError:break # break if end is reached
         except Exception:
             print('\n'+traceback.format_exc())
-            output(2,'\n [18]ERROR','<red>', 'Please copy & paste this on GitHub > Issues & provide a short explanation.')
+            output(2,'\n [20]ERROR','<red>', 'Please copy & paste this on GitHub > Issues & provide a short explanation.')
             input('\nPress any key to close ...')
             exit()
     except KeyError:
-        output(2,'\n [19]ERROR','<red>', "Couldn't find any scrapeable media at all!\n This most likely happend because you're not following the creator, your authorisation token is wrong\n or the creator is not providing unlocked content.")
+        output(2,'\n [21]ERROR','<red>', "Couldn't find any scrapeable media at all!\n This most likely happend because you're not following the creator, your authorisation token is wrong\n or the creator is not providing unlocked content.")
         input('\nPress any key to close ...')
         exit()
     if remember == 'True' and recent > int(total_photos+total_videos) * 0.2:
